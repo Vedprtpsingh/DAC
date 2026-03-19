3e1b05734d5536aea13397dea681b8534d0ec00e
vedprtpsingh <singhvedpratap14@gmail.com>
2026-03-19 22:36:33 +0530

    3

--- VedAbhiCPP/CPPAssignment1DArray02/AirlineSeatBookingSystem.cpp
+++ VedAbhiCPP/CPPAssignment1DArray02/AirlineSeatBookingSystem.cpp
@@ -1,10 +1,49 @@
 #include<iostream>
 using namespace std;
-
 #define SIZE 50
-
-void searchtocheck(int arr[],int search);
-
+void searchtocheck(bool arr[]);
+void display(bool arr[]);
 int main(){
+    bool arr[SIZE];
+    for(int i=0;i<SIZE;i++){
+        arr[i]=rand()%2;
+    }
+    display(arr);
+    cout<<"Avalible seats: ";
+    for(int i=0;i<SIZE;i++){
+        if(arr[i]==0){
+            cout<<i+1<<" ";
+        }
+    }
+    cout<<"Book First Avalible Seat: ";
+    for(int i=0;i<SIZE;i++){
+        if(arr[i]==0){
+            arr[i]=1;
+            cout<<"Seat "<<i+1<<" Book Conformed:";
+            break;
+        }
+    }
+    display(arr);
+
+    searchtocheck(arr);
+    return 0;
+}
+void display(bool arr[]){
+    cout<<"Airline Seat Display: ";
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     
+}
+void searchtocheck(bool arr[]){
+    cout<<"Enter Element To Search: ";
+    int s;
+    cin>>s;
+    if(arr[s-1]==1){
+        cout<<"Seat is not avalible";
+    }else{
+        cout<<"Seat is avalible";
+    }
 }
---
+++ VedAbhiCPP/CPPAssignment1DArray02/ShoppingCartDiscountSystem.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+#define SIZE 8
+
+void searchtocheck(int arr[],int search);
+
+int main(){
+
+}
