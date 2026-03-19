fb39bad4c60769527d15eb896ad3216dc1685f19
VED PRATAP SINGH <119805462+Vedprtpsingh@users.noreply.github.com>
2026-03-19 20:33:51 +0530

    Delete Ved directory

--- Ved/CoreJavaLectureProblem01/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module CoreJavaLectureProblem01 {
-}
--- Ved/LabAssignment03/src/electricityBillGenerator/ElectricityApp.java
+++
@@ -1,9 +0,0 @@
-package electricityBillGenerator;
-
-public class ElectricityApp {
-	public static void main(String[] args) {
-		ElectricityConnection ec=new ElectricityConnection(1001, "Abhi", 402);
-		ElectricityConnection.BillCalculator inner=ec.new BillCalculator();
-		System.out.println(inner.calculator(ec.getUnitsConsumed()));
-	}
-}
